4c237d5f94aa43f35490bc23fccc4b111c556580
Jason J <admin@ovrhere.com>
2014-03-06 13:03:27 -0400

    Ref. out info from index, made use of session, added layout css
    [Add][Ref]: Added css layout for the app, the linkedin_button css was
     refactored to its own file, added css for linkin_card_layout.
    [Mod][Ref]: Added functionality to show the user logged in & to 
    insert/remove the login prompt.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+//Compatibility functions for IE
+
+if(typeof String.prototype.trim !== 'function') {
+  String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g, ''); 
+  };
+}
+/*
+ * Alternatively:
+ * if (element.addEventListener) {
+        element.addEventListener('event', function, false);
+    } else if (element.attachEvent) {
+        element.attachEvent('event',  function);
+    } else {
+        element['onevent'] = function;
+    } 
+ */
+if (typeof Document.prototype.addEventListener  !== 'function' ) {
+        Document.prototype.addEventListener = function(evt, func, bubble){
+            evt = 'on'+evt;
+            if (this.attachEvent){
+                this.attachEvent(evt, func);
+            } else {
+                this[evt] = func;
+            }
+        };
+}
+
 /** 
  * The entry point into the application. 
  * Always call last.
@@ -20,13 +48,24 @@ var myConnectionsMap = {
          * /v1/people/~:(id,pictureUrl,firstName,lastName,location:(name,country:(code)),numConnectionsCapped)*/
         userInfo: {
             id: 0, pictureUrl: 0, firstName: 0, lastName: 0, 
-            location: {name:0, country: { code: 0},}, numConnectionsCapped: 0        
+            location: {name:0, country: { code: 0}}, numConnectionsCapped: 0        
         }
     }
 };
 /** Initializes the application. */
 myConnectionsMap.init = function(){
     this.map = new MapDisplay();
+};
+
+/** Creates and displays the login splash. */
+myConnectionsMap.linkedin.showLoginSplash = function(){
+    var inConnect = new LinkedInConnect('linkedin-connect-button', 'auth.php');
+    var msg = document.createElement('div');
+        msg.innerHTML = '(Opens in a new window)';
+    var splash = document.getElementById('connect-splash-inner');
+        splash.appendChild(inConnect.getButton());
+        splash.appendChild(msg);
+        splash.style.display = '';    
 };
 
 /** Sets the main user info and then shows it.
@@ -46,10 +85,45 @@ myConnectionsMap.linkedin.setUser = function (userJson){
 };
 /** Shows the user on the display. */
 myConnectionsMap.linkedin.showUser = function(){
+    var truncate = function(input, length){
+        return  input.length > length ?
+                input.substr(0, length-3)+'...' :
+                input;
+    };
+    var userNameLength = 44;
+    var userLocLength = 50;
+    var fullname  =  this.userInfo.firstName+' '+this.userInfo.lastName;
+    var location = this.userInfo.location.name;
+    var title = fullname+', '+location;
+    
     var userPic = document.createElement('img');
+        userPic.setAttribute('id', 'linkedin-user-pic');
         userPic.setAttribute('src', this.userInfo.pictureUrl);
-        userPic.setAttribute('title', this.userInfo.firstName+' '+this.userInfo.lastName);
-    document.getElementById('linkedin-display').appendChild(userPic);
+        userPic.setAttribute('title', title.trim());
+    
+    var userName = document.createElement('div');
+        userName.setAttribute('id', 'linkedin-user-name');
+        userName.innerHTML = truncate(fullname, userNameLength);
+    var userLoc = document.createElement('div');
+        userLoc.setAttribute('id', 'linkedin-user-location');
+        userLoc.innerHTML = truncate(location, userLocLength);
+        
+    var userInfo = document.createElement('div');
+        userInfo.setAttribute('id', 'linkedin-user-info');
+        userInfo.appendChild(userName);
+        userInfo.appendChild(userLoc);
+        
+    // Remove splash
+    var splash = document.getElementById('connect-splash');
+        splash.parentNode.removeChild(splash);    
+        
+    //remove hidden style.
+    document.getElementById('app-controls').style.display = '';
+    var display = document.getElementById('linkedin-display');
+        display.style.display = '';
+        display.appendChild(userPic);
+        display.appendChild(userInfo);
+    
 };
 
 myConnectionsMap.init();
