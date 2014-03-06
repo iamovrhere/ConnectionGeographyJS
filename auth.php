375d63fca851ac8726fd49304946b6c2b77cb3ec
Jason J <admin@ovrhere.com>
2014-03-06 13:00:52 -0400

    [Fix]: Fixed the logout function of the auth, cached profile in Session
    [Add]: Added getprofile_func.php

--- auth.php
+++ auth.php
@@ -1,13 +1,14 @@
 <?php
 require_once 'configure.php';
+require_once 'getprofile_func.php';
 
-/** @version 0.1.0-20140305 */
+/** @version 0.1.2-20140306 */
 
 //redirect uri supplied to linkedin for auth. 
 define('REDIRECT_URI', 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME']);
 
 if (isset($_GET['logout']) && $_GET['logout']){
-    Session::endSession();
+    Session::endSession(SESSION_NAME);
     echo 'logout';
     exit;
 }
@@ -25,37 +26,37 @@ if (!$linkedin->authCheck()){
 set_error_handler(array('ErrorHandling', 'errorHandlerException'), E_NOTICE | E_WARNING);
 $user_json =  0;
 try {
-    $user_json = LinkedInAPI::get(LINKEDIN_PROFILE_REQUEST);
+    $user_json = getUserProfileJSON();
 } catch (Exception $e){
-    Session::endSession();
+    Session::endSession(SESSION_NAME);
     //reload
     header("Location: ".REDIRECT_URI); 
     exit;
-}
+} 
 restore_error_handler();
 
-$user = json_decode($user_json);
+
+
+/* $user = json_decode($user_json);
 print "Hello <img src='$user->pictureUrl' title='$user->firstName $user->lastName' /> $user->firstName $user->lastName. (id: $user->id)";
         $_location = $user->location;
         $_country = $_location->country;
         print " | Location: $_location->name, $_country->code";
         print " | Capped: " .($user->numConnectionsCapped ? 'yes' : 'no');
         
-echo '<br />';
+echo '<br />'; */
 
 ?>
 <script type="text/javascript">
     var response = <?php echo $user_json; ?>;
 function myclose(){
-    console.log('response: %s', response);
-    console.log('response: %s', JSON.stringify(response));
+    //console.log('response: %s', response);
+    //console.log('response: %s', JSON.stringify(response));
     if (window.opener){
         window.opener   .myConnectionsMap
                         .linkedin
                         .setAndShowUser.call(window.opener.myConnectionsMap.linkedin, response);
     }
-    //window.opener.myConnectionsMap.linkedin.reconnect();
-    //window.opener.testcomplete();
     self.close();
 }
 myclose();
