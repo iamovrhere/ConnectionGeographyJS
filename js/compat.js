f1d4d8caaef0df28a1c60256e4ebd738874ca66d
Jason J <admin@ovrhere.com>
2014-03-12 10:27:15 -0400

    [Add][Mod]: Added compatibility  for Object.create

--- js/compat.js
+++ js/compat.js
@@ -29,3 +29,11 @@ if (typeof console.log !== 'function'){
      console.log = function(){};
 }
 
+if(!Object.create){
+    Object.create=function(o){
+        function F(){}
+        F.prototype=o;
+        return new F();
+    };
+};
+
